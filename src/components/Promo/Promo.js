c2f8100d98a9c44065277af4dad8b6e700bb0b37
Vladislav Kozhevatov <envydef@gmail.com>
2021-07-06 15:00:48 +0300

    feat: add slick slider

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -2,40 +2,46 @@ import React from 'react';
 import Slider from 'react-slick';
 import './Promo.css';
 import ad from '../../images/image.jpg';
+import SaleItem from '../SaleItem/SaleItem';
 
 const Promo = () => {
-    let settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      };
-    return(
-        <section className="promo">
-            <Slider className='promo__slider' {...settings}>
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  return (
+    <section className="promo">
       <div>
-        <h3><img src={ad} /></h3>
+      <Slider className='promo__slider' {...settings}>
+        <div className='promo__sliderItem'>
+          <img src={ad} />
+        </div>
+        <div className='promo__sliderItem'>
+          <img src={ad} />
+        </div> 
+        <div className='promo__sliderItem'>
+          <img src={ad} />
+        </div>
+        <div className='promo__sliderItem'>
+          <img src={ad} />
+        </div>
+        <div className='promo__sliderItem'>
+          <img src={ad} />
+        </div>
+        <div className='promo__sliderItem'>
+          <img src={ad} />
+        </div>
+      </Slider>
       </div>
       <div>
-        <h3><img src={ad} /></h3>
+      <SaleItem />
       </div>
-      <div>
-        <h3><img src={ad} /></h3>
-      </div>
-      <div>
-        <h3><img src={ad} /></h3>
-      </div>
-      <div>
-        <h3><img src={ad} /></h3>
-      </div>
-      <div>
-        <h3><img src={ad} /></h3>
-      </div>
-    </Slider>
-    
-        </section>
-    );
+
+    </section>
+  );
 }
 
 export default Promo;
